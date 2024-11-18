2ccc3c4cd3765d09914b982fda363e522dce53b5
Fernanda Tomita <ft.manchon@gmail.com>
2024-11-18 16:49:53 -0300

    criando componente details e implementando rotas para exibir outras paginas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,6 +14,7 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
   }
